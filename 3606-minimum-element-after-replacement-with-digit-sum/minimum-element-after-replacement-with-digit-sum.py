f2109d7799e6c57007da3fbcf221052da416c347
kotlanandu525 <nandukotla525@gmail.com>
2026-05-29 09:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (21.61%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
-        temp=[]
+        ans=float("inf")
         for num in nums:
             s=0
             while num>0:
                 s+=num%10
                 num//=10
-            temp.append(s)
-        return min(temp)
+            ans=min(s,ans)
+        return ans
         
