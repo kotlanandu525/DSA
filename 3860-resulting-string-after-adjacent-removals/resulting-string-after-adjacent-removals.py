933ceb35a83ecc904e6d328eb780547df9c65a15
kotlanandu525 <nandukotla525@gmail.com>
2026-07-05 22:50:49 +0530

    Time: 612 ms (51.35%) | Memory: 21 MB (85.13%) - LeetSync

--- 3860-resulting-string-after-adjacent-removals/resulting-string-after-adjacent-removals.py
+++ 3860-resulting-string-after-adjacent-removals/resulting-string-after-adjacent-removals.py
@@ -3,11 +3,9 @@ class Solution:
         
         st=[]
         for ch in s:
-            if not st:
-                st.append(ch)
-            else:
+            if st:
                 if abs(ord(st[-1])-ord(ch))==1 or abs(ord(st[-1])-ord(ch))==25:
                     st.pop()
-                else:
-                    st.append(ch)
+                    continue
+            st.append(ch)
         return "".join(st)
