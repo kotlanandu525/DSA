b91bfd65be5cffbe45e693a51ad07613b28dad95
kotlanandu525 <nandukotla525@gmail.com>
2026-07-12 13:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (43.26%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        c=0
+        for log in logs:
+            if log=='../':
+                if c==0:
+                    c=0
+                else:
+                    c-=1
+            elif log=='./':
+                continue
+            else:
+                c+=1
+        return c
